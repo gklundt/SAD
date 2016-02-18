d4e8a3c98e0faae1b49026a2a339eafbb3484de4
Gordon Klundt <gklundt@gmail.com>
2016-02-17 20:40:26 -0600

    after presentation

--- main.cpp
+++ main.cpp
@@ -10,19 +10,20 @@ int main() {
 
     vector<TokenBag> v;
     TokenBag t;
-    t.setTokens((char *) "big flood\napple pie\nApple , Fiona\nbanana bunch\ncherry pie\n123 pilots Pilots\ngoat herder\ndog trainer's coat");
+    //t.setTokens((char *) "big flood\napple pie\nApple , Fiona\nbanana bunch\ncherry pie\n123 pilots Pilots\ngoat herder\ndog trainer's coat");
     //t.setTokens((char *) "a\nA\nb\nB\nC\nD\nc\nd");
     //t.setTokens((char *) "aAa AaA\nAAA aaa\nAaA aAa\naaa AAA\nC\nD\nc\nd");
     //t.setTokens((char *)"my country 'tis of thee\nsweet land of liberty\nof thee I sing");
+    t.setTokens((char *) "Software Architecture and Design\naA aaa\naa Aaa\nAa Aaa\nAA    aaa");
     v.push_back(t);
 
     Filter *p = new Parse;
-    Filter *s = new Shift;
-    Filter *a = new Sort;
+    Filter *a = new Shift;
+    Filter *s = new Sort;
 
     vector<TokenBag> out = p->Process(v);
-    out = a->Process(out);
     out = s->Process(out);
+    out = a->Process(out);
 //    out = a->Process(s->Process(p->Process(out)));
 //    out = a->Process(s->Process(p->Process(out)));
 //    out = a->Process(s->Process(p->Process(out)));
